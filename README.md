b008b85220d6113078675dc8150c4d9ffa5f0191
Joshua Jacobs <joshua.jacobs@mayo.edu>
2020-08-01 19:47:01 -0500

    Firstly

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # foldable-bookmark
 Simple application to create a foldable bookmark from an image
+
+## Usage
+
+```python image_to_pages.py --picture=images/Earth.png```
+
+## Output
+
+Original:
+
+<img src="./images/Earth.jpg" alt="drawing" width="200"/>
+
+Front Side (Page 1):
+
+<img src="./images/Page_1.png" alt="drawing" width="400"/> 
+
+Back Side (Page 2):
+
+<img src="./images/Page_2.png" alt="drawing" width="400"/>
---
+++ image_to_pages.py
@@ -0,0 +1,58 @@
+import argparse
+import sys
+
+import numpy as np
+from PIL import Image
+
+
+def main(args):
+    """
+    
+
+    Args:
+        args ([type]): [description]
+    """
+    img_path = args.picture
+    img = Image.open(img_path)
+    min_size = min(img.size)
+    img = img.crop([0, 0, min_size, min_size])
+    img_array = np.array(img)
+
+    page_1 = np.ones([min_size * 2, min_size * 2, 3]) * 255
+    page_2 = np.ones([min_size * 2, min_size * 2, 3]) * 255
+
+    for i in range(3):
+        tmp = np.tril(img_array[:, :, i], k=0)
+        tmp[np.triu_indices(min_size)] = 255
+        page_1[min_size:, :min_size, i] = tmp
+
+    for i in range(3):
+        tmp_1 = np.triu(img_array[:, :, i], k=1)
+        tmp_1[np.tril_indices(min_size, k=0)] = 255
+        tmp_1 = np.fliplr(tmp_1)
+
+        tmp_2 = np.tril(tmp_1, k=0)
+        tmp_2[np.triu_indices(min_size, k=1)] = 255
+        tmp_2 = np.fliplr(tmp_2)
+
+        tmp_3 = np.triu(tmp_1, k=1)
+        tmp_3[np.tril_indices(min_size, k=0)] = 255
+        tmp_3 = np.fliplr(np.flip(tmp_3))
+
+        page_2[min_size:, :min_size, i] = tmp_2
+        page_2[:min_size, min_size:, i] = tmp_3
+
+    page_1 = Image.fromarray(page_1.astype(np.uint8))
+    page_2 = Image.fromarray(page_2.astype(np.uint8))
+
+    page_1.save("Page_1.png")
+    page_2.save("Page_2.png")
+
+    print(img_path)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--picture")
+    args = parser.parse_args()
+    main(args)
